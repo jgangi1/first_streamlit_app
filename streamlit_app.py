791a1c2a7f2b03e5cbc4650297db920e5a914b29
jgangi1 <145722874+jgangi1@users.noreply.github.com>
2023-09-21 15:50:02 -0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('my parents healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega3 & Blueberry oatmeal')
+streamlit.text('Kale & Spinach and Orange juice')
