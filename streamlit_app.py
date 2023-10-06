9f147966ec5bf3c85eb5b3ae669899e9c9d65494
jgangi1 <145722874+jgangi1@users.noreply.github.com>
2023-10-05 23:09:22 -0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
        streamlit.header("The fruit_load_list contains:")
-def  get_fruit_load_list():
+def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
@@ -42,7 +42,7 @@ if streamlit.button('Get Fruit Load List'):
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
-def  insert_row_snowflake(new_fruit):
+def insert_row_snowflake(new_fruit):
      with my_cnx.cursor() as my_cur:
           my_cur.execute("insert into fruit_load_list_values ('from streamlit')")
           return "Thanks for adding "+ new_fruit
