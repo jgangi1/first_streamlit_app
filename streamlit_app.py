eb7a639349899a4564bc0f3fa721299c5e453519
jgangi1 <145722874+jgangi1@users.noreply.github.com>
2023-10-05 23:15:58 -0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ try:
    else:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
+      #new section to display api response  
 streamlit.header("The fruit_load_list contains:")
 def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
