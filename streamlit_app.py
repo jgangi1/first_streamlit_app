33f70baffa061dae90906ba4086df7b058e2eaba
jgangi1 <145722874+jgangi1@users.noreply.github.com>
2023-10-05 22:57:05 -0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
-   
+#new section to display api response  
 streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -30,9 +30,8 @@ try:
        streamlit.error('Please select a fruit to get information')
    else:
        back_from_function = get_fruityvice_data(fruit_choice)
-       #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-       #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        streamlit.dataframe(back_from_function)
+      streamlit.stop()
 except URLError as e:
   streamlit.error()
 streamlit.header("The fruit_load_list contains:")
