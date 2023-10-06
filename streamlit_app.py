08930778a6c11e1de16c88d186eb16f33d3c0ea4
jgangi1 <145722874+jgangi1@users.noreply.github.com>
2023-10-05 21:59:19 -0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error
-import URLError
+from urllib.error import URLError
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
